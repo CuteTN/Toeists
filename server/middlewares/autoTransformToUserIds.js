f4bcb3684f01d222ef912ff4b815100e3663c4f7
CuteTN <quantiennghia001@gmail.com>
2021-10-23 01:53:32 +0700

    Add Auto transform to userId middleware

---
+++ server/middlewares/autoTransformToUserIds.js
@@ -0,0 +1,53 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import { validate as validateEmail } from 'email-validator'
+import { User } from '../models/user.js'
+import { findUserByIdentifier } from '../services/users.js'
+
+/** @type {(...selectors: Selector[]) => express.RequestHandler} */
+export const autoTransformToUserIdsFn = (...selectors) => async (req, res, next) => {
+  let targetedParent = null;
+  const users = await User.find();
+
+  selectors.forEach(selector => {
+    try { targetedParent = selector[0](req, res); }
+    catch { targetedParent = null }
+
+    if (!targetedParent)
+      return;
+
+    const strToConvert = targetedParent[selector[1]];
+
+    // null
+    if (!strToConvert)
+      return;
+
+    // already an object Id
+    if (mongoose.isValidObjectId(strToConvert))
+      return;
+
+    // username or email
+    const user = findUserByIdentifier(strToConvert, users);
+    if (!user?.id)
+      return;
+
+    targetedParent[selector[1]] = user.id;
+  })
+
+  next?.();
+}
+
+/**
+ * @typedef {"id" | "username" | "email"} UserTransform
+ */
+
+/**
+ * @callback ParentSelector
+ * @param {express.Request<ParamsDictionary, any, any, QueryString.ParsedQs, Record<string, any>>} req
+ * @param {express.Response<any, Record<string, any>, number>?} res
+ * @returns {any}
+ */
+
+/**
+ * @typedef {[ParentSelector, string]} Selector
+ */
--- server/routes/userConnection.js
+++ server/routes/userConnection.js
@@ -1,10 +1,15 @@
 import express from "express";
 import * as controllers from "../controllers/userConnection.js";
 import { authorize } from "../middlewares/authorization.js";
+import { autoTransformToUserIdsFn } from "../middlewares/autoTransformToUserIds.js";
 
 export const userConnectionRouter = express.Router();
 
-userConnectionRouter.post("/follow", authorize, controllers.follow);
-userConnectionRouter.post("/block", authorize, controllers.block);
-userConnectionRouter.delete("/unfollow", authorize, controllers.unfollow);
-userConnectionRouter.delete("/unblock", authorize, controllers.unblock);
+const reqToUserIds = autoTransformToUserIdsFn(
+  [req => req.body, "toUserId"]
+);
+
+userConnectionRouter.post("/follow", authorize, reqToUserIds, controllers.follow);
+userConnectionRouter.post("/block", authorize, reqToUserIds, controllers.block);
+userConnectionRouter.delete("/unfollow", authorize, reqToUserIds, controllers.unfollow);
+userConnectionRouter.delete("/unblock", authorize, reqToUserIds, controllers.unblock);
