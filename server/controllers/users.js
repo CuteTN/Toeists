4fc2b22ccdefe743805545bf34f51092e7534500
CuteTN <quantiennghia001@gmail.com>
2021-10-23 15:00:35 +0700

    Add Update user API

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,6 +1,8 @@
+import bcrypt from 'bcryptjs'
 import { User } from "../models/user.js"
 import express from 'express'
 import { httpStatusCodes } from "../utils/httpStatusCode.js";
+import mongoose from 'mongoose'
 
 /** @type {express.RequestHandler} */
 export const getAllUsers = async (req, res, next) => {
@@ -13,8 +15,35 @@ export const getUserById = async (req, res, next) => {
   const { id } = req.params;
   const user = await User.findById(id);
 
-  if(!user)
+  if (!user)
     return res.status(httpStatusCodes.notFound).send("User not found");
 
   return res.status(httpStatusCodes.ok).send(user);
+}
+
+/** @type {express.RequestHandler} */
+export const updateUser = async (req, res, next) => {
+  const { id } = req.params;
+  const updatingData = req.body;
+
+  if (updatingData.hashedPassword)
+    delete updatingData.hashedPassword;
+
+  if (updatingData.password) {
+    const hashedPassword = await bcrypt.hash(updatingData.password, 12);
+    updatingData.hashedPassword = hashedPassword;
+    delete updatingData.password;
+  }
+
+  if (!mongoose.isValidObjectId(id))
+    return res.status(httpStatusCodes.badRequest).json({ message: "Invalid ID." });
+
+  const existedUser = await User.findById(id);
+  if (!existedUser)
+    return res.sendStatus(httpStatusCodes.notFound);
+
+  try { var updatedUser = await User.findByIdAndUpdate(id, updatingData, { new: true }) }
+  catch (error) { return res.status(httpStatusCodes.badRequest).json(error) }
+
+  return res.status(httpStatusCodes.ok).json(updatedUser);
 }
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,14 @@
 import express from "express";
 import * as controller from "../controllers/users.js";
+import { autoTransformToUserIdsFn } from '../middlewares/autoTransformToUserIds.js'
+import { authorize } from '../middlewares/authorization.js'
 
 export const usersRouter = express.Router();
 
+const reqParamsToUserId = autoTransformToUserIdsFn(
+  [req => req.params, 'id']
+)
+
 usersRouter.get('/', controller.getAllUsers);
-usersRouter.get('/:id', controller.getUserById);
+usersRouter.get('/:id', reqParamsToUserId, controller.getUserById);
+usersRouter.put('/:id', authorize, reqParamsToUserId, controller.updateUser);
