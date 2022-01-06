906adb84c05ad023c5ec9a01cd8166bad2951afb
CuteTN <quantiennghia001@gmail.com>
2022-01-07 02:39:20 +0700

    Add isMyMessage function for MessageView

--- client/src/pages/ChatPage/MessagesView/MessagesView.js
+++ client/src/pages/ChatPage/MessagesView/MessagesView.js
@@ -1,13 +1,33 @@
+import Text from "antd/lib/typography/Text";
 import React from "react";
+import { useAuth } from "../../../contexts/authenticationContext";
 //others
 import "./style.css";
 
 const MessagesView = ({ conversation }) => {
+  const { signedInUser } = useAuth();
+
   /** @type {any[]} */
   const messages = React.useMemo(() => conversation?.messages, [conversation])
 
+  const isMyMessage = (message) => {
+    return message?.senderId === signedInUser?._id
+  }
+
   return (
-    <div className="message-form-wrapper">{JSON.stringify(messages?.map(m => m.text))}</div>
+    <div className="message-form-wrapper">
+      {messages?.map((msg, i) =>
+        <div key={i}>
+          {isMyMessage(msg) ?
+            <Text strong>{msg.text}</Text>
+            :
+            <Text>{msg.text}</Text>
+          }
+          <br />
+        </div>
+      )}
+
+    </div>
   )
 };
 export default MessagesView;
