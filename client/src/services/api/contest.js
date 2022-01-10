e0ef47455c09f4f1bf7dbb8198c0cf74821b6903
hellanngel108 <18520155@gm.uit.edu.vn>
2022-01-10 18:13:02 +0700

    add delete contest

--- client/src/services/api/contest.js
+++ client/src/services/api/contest.js
@@ -4,3 +4,5 @@ export const createContestPart = (contestPart) =>
   apiService.post("/api/contest-parts", contestPart);
 export const fetchAContest = (contestID) =>
   apiService.get(`api/contest-parts/${contestID}`);
+export const deleteContest = (contestID) =>
+  apiService.delete(`api/contest-parts/${contestID}`);
