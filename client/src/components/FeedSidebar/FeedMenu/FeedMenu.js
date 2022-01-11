ba8effd1bbfbb5071b816a2d31960cee0d49ba9a
CuteTN <quantiennghia001@gmail.com>
2022-01-11 11:36:28 +0700

    Fix sidebar

--- client/src/components/FeedSidebar/FeedMenu/FeedMenu.js
+++ client/src/components/FeedSidebar/FeedMenu/FeedMenu.js
@@ -4,11 +4,15 @@ import {
   UserOutlined,
   TeamOutlined,
   CheckSquareOutlined,
+  GlobalOutlined
 } from "@ant-design/icons";
 import styles from "../styles.js";
 import { Link } from "react-router-dom";
+import { useAuth } from "../../../contexts/authenticationContext.js";
 
 function FeedMenu({ user }) {
+  const { signedInUser } = useAuth();
+
   return (
     <Menu
       mode="inline"
@@ -19,21 +23,32 @@ function FeedMenu({ user }) {
         fontSize: "1rem",
       }}
     >
-      <Menu.Item
-        key="username"
-        style={styles.item}
-        icon={<UserOutlined style={{ fontSize: "1.4rem" }} />}
-      >
-        <Link to={`/userinfo/${user?._id}`}>My Profile</Link>
-      </Menu.Item>
+      {signedInUser &&
+        <Menu.Item
+          key="username"
+          style={styles.item}
+          icon={<UserOutlined style={{ fontSize: "1.4rem" }} />}
+        >
+          <Link to={`/userinfo/${signedInUser?._id}`}>My Profile</Link>
+        </Menu.Item>
+      }
 
-      <Menu.Item
+      {/* <Menu.Item
         key="friends"
         style={styles.item}
         icon={<TeamOutlined style={{ fontSize: "1.4rem" }} />}
       >
         <Link to="/friends">Following</Link>
+      </Menu.Item> */}
+
+      <Menu.Item
+        key="feed"
+        style={styles.item}
+        icon={<GlobalOutlined style={{ fontSize: "1.4rem" }} />}
+      >
+        <Link to="/feed">News feed</Link>
       </Menu.Item>
+
       <Menu.Item
         key="contests"
         style={styles.item}
